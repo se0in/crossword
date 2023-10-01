37afb8cce13c01ef2bb23a13ed061c16485fec35
se0in <tjfls0@naver.com>
2023-10-01 21:27:25 +0900

    경로 수정

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 // crossword.html 파일 불러오기
-const CROSS_HTML = "/../crossword.html";
+const CROSS_HTML = "/../sub/crossword.html";
 const CROSS_ID = document.getElementById("crossword");
 fetch(CROSS_HTML)
   .then((response) => response.text())
@@ -9,19 +9,3 @@ fetch(CROSS_HTML)
   .catch((error) => {
     console.log("파일을 가져오는 동안 오류가 발생했습니다,", error);
   });
-// $(document).ready(function(){});
-
-/* window.addEventListener('resize', function(){
-  backgroundVisibility();
-});
-
-function backgroundVisibility(){
-  let screenHeight = window.innerHeight;
-  let backScreenIconBox = document.querySelectorAll('.background-icon');
-  let backTaskbar = document.querySelector('.background-taskbar');
-
-  if (screenHeight < 810) {
-    console.log('hjhj');
-    backScreenIconBox.style.display = 'none';
-  }
-} */
